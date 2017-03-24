620825aae12a1498e95c99d07f0b50892f691e53
ggwilliams <gabithan@icloud.com>
2017-03-24 14:16:55 -0400

    Initial Commit

---
+++ assets/scripts/config.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const config = {
+  apiOrigins: {
+    production: 'https://salty-ravine-27099.herokuapp.com/',
+    development: 'http://localhost:4741/'
+  },
+};
+
+module.exports = config
